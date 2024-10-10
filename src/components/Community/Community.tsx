594fc0c21ef4e5ad0ecf04f1f7df730c8ebf8d98
Bright Udoette <brightudoette@gmail.com>
2024-10-10 07:53:18 +0100

    Change the color from value of the elements from being hardcoded to fecthing the values from a theme file using Chakra useTheme hook

--- src/components/Community/Community.tsx
+++ src/components/Community/Community.tsx
@@ -1,7 +1,8 @@
-import { Box, Text, Flex, Icon, HStack } from "@chakra-ui/react";
+import { Box, Text, Flex, Icon, HStack, useTheme } from "@chakra-ui/react";
 import { FaChevronRight } from "react-icons/fa";
 
 const Community: React.FC = () => {
+  const theme = useTheme();
   // List of community categories with posts and replies
   const communitySections = [
     { title: "GENERAL COMMENTS", posts: 33, replies: 25 },
@@ -12,32 +13,47 @@ const Community: React.FC = () => {
   ];
 
   return (
-    <Box p={4}>
+    <Box p={4} color={theme.colors.colorWorkSpace}>
       {communitySections.map((section, index) => (
         <Flex
           key={index}
           p={4}
-          my={2}
+          cursor="pointer"
+          my={3}
           alignItems="center"
           justifyContent="space-between"
           borderWidth="1px"
-          borderRadius="md"
-          boxShadow="md"
-          _hover={{ bg: "gray.50" }}
+          borderColor={theme.colors.primaryBorderColor}
+          borderRadius="2xl"
+          _hover={{ bg: theme.colors.primaryBorderColor }}
         >
           <HStack spacing={4}>
-            <Box>
-              <Text fontWeight="bold" fontSize="lg">
+            <Box fontWeight="bold">
+              <Text
+                fontSize={theme.fontSizes.md}
+                textTransform="uppercase"
+                mb={2}
+              >
                 {section.title}
               </Text>
-              <Text fontSize="sm" color="gray.500">
-                {section.posts} posts • {section.replies} replies
+              <Text
+                fontSize={theme.fontSizes.sm}
+                color={theme.colors.purpleTextColor}
+              >
+                {section.posts} posts{" "}
+                <Text as="span" mx={1} color="#627D98">
+                  •
+                </Text>{" "}
+                {section.replies} replies
               </Text>
             </Box>
           </HStack>
 
-          {/* Chevron Icon on the far right */}
-          <Icon as={FaChevronRight} boxSize={6} color="gray.400" />
+          <Icon
+            as={FaChevronRight}
+            boxSize={4}
+            color={theme.colors.purpleTextColor}
+          />
         </Flex>
       ))}
     </Box>
